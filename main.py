b3d37cccab797239c183f914c67221a3e427a585
lin <ashin8090@163.com>
2022-09-19 22:32:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import re
 import sys
 
 import requests
@@ -7,8 +8,44 @@ import urllib3
 urllib3.disable_warnings()
 
 
+def get_url_ip(url):
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Pragma': 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/555.0 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/555.0 Edg/105.0.0.0',
+    }
+
+    params = {
+        'lang': 'zh-CN',
+    }
+
+    response = requests.get(f'http://ip-api.com/json/{url}', headers=headers, params=params, verify=False)
+
+    try:
+        return response.json()
+    except Exception as e:
+        print(f'Error => get_url_ip[{url}]', e, response.text)
+        return response.text
+
+
 def get_github_ips():
-    response = requests.get('https://raw.hellogithub.com/hosts', verify=False)
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,en-GB;q=0.6',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Pragma': 'no-cache',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/555.0 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/555.0 Edg/105.0.0.0',
+    }
+
+    response = requests.get('https://raw.hellogithub.com/hosts', headers=headers, verify=False)
     if response.status_code == 200:
         return response.text
     else:
@@ -36,7 +73,7 @@ def operate_custom_host(operate_type: str = 'get', hosts: list = None):
         'Pragma': 'no-cache',
         'Cache-Control': 'no-cache',
         'Accept': '*/*',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/555.0 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/555.0 Edg/105.0.0.0',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Origin': 'https://s.miwifi.com',
         'X-Requested-With': 'com.xiaomi.smarthome',
@@ -64,6 +101,19 @@ def operate_custom_host(operate_type: str = 'get', hosts: list = None):
         return response.text
 
 
+def cover_ip(hosts):
+    for i in range(len(hosts)):
+        item = hosts[i]
+        if not re.match(r'^127\.0\.0\.1', item):
+            item_temp = item.split(' ')
+            item_url = item_temp[-1]
+            res = get_url_ip(item_url)
+            ip = res.get('query', '') if type(res) == dict else ''
+            if ip:
+                hosts[i] = f'{ip} {item_url}'
+    return hosts
+
+
 if __name__ == '__main__':
     base_data = sys.argv[1]
     try:
@@ -83,6 +133,8 @@ if __name__ == '__main__':
     custom_hosts = get_custom_hosts_res.get('hosts', []) if type(get_custom_hosts_res) == dict else []
     github_ips_start = '127.0.0.1 le.github-ips.start'
     github_ips_end = '127.0.0.1 le.github-ips.end'
+
+    # print('custom_hosts => ', json.dumps(custom_hosts, indent=2))
     try:
         if custom_hosts.index(github_ips_start) >= 0:
             # 删除旧 github_ips 内容
@@ -90,8 +142,15 @@ if __name__ == '__main__':
                 custom_hosts.index(github_ips_start):custom_hosts.index(github_ips_end) + 1]
     except:
         pass
-    github_ips = list(filter(lambda x: x, get_github_ips().split('\n')))
+
+    custom_hosts = cover_ip(custom_hosts)
+    # print('new custom_hosts => ', json.dumps(custom_hosts, indent=2))
+
+    github_ips = list(filter(lambda x: re.match(r'^\d', x), get_github_ips().split('\n')))
     print('github_ips => ', json.dumps(github_ips, indent=2))
+    github_ips = cover_ip(github_ips)
+    print('new github_ips => ', json.dumps(github_ips, indent=2))
+
     # 添加 github_ips 内容的开始、结束元素，方便下次仅更新 github_ips 部分的内容
     github_ips.insert(0, github_ips_start)
     github_ips.insert(len(github_ips) + 1, github_ips_end)
