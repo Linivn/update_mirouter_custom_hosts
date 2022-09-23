70096a28f30c2cc29fa15bcfbb5b40c8a0a349e2
lin <ashin8090@163.com>
2022-09-23 23:07:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -45,11 +45,12 @@ def get_github_ips():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/555.0 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/555.0 Edg/105.0.0.0',
     }
 
-    response = requests.get('https://raw.hellogithub.com/hosts', headers=headers, verify=False)
-    if response.status_code == 200:
+    response = requests.get('https://raw.hellogithub.com/hosts.json', headers=headers, verify=False)
+    try:
+        return response.json()
+    except Exception as e:
+        print(f'Error => get_github_ips', e, response.text)
         return response.text
-    else:
-        return ''
 
 
 def operate_custom_host(operate_type: str = 'get', hosts: list = None):
@@ -104,8 +105,8 @@ def operate_custom_host(operate_type: str = 'get', hosts: list = None):
 def cover_ip(hosts):
     for i in range(len(hosts)):
         item = hosts[i]
-        if not re.match(r'^127\.0\.0\.1', item):
-            item_temp = item.split(' ')
+        if not re.match(r'127\.0\.0\.1', str(item)):
+            item_temp = item.split(' ') if type(item) == str else item
             item_url = item_temp[-1]
             res = get_url_ip(item_url)
             ip = res.get('query', '') if type(res) == dict else ''
@@ -116,11 +117,62 @@ def cover_ip(hosts):
 
 if __name__ == '__main__':
     base_data = sys.argv[1]
+    site_list = sys.argv[2]
+
     try:
         base_data = json.loads(base_data)
     except:
         raise ValueError(
             '请配置正确的 base_data 参数，如 \'{"appId": "xxx", "deviceId": "xxx", "clientId": "xxx", "scope": "xxx", "token": "xxx"}\'')
+
+    try:
+        if site_list:
+            site_list = json.loads(site_list)
+        else:
+            site_list = [
+                "alive.github.com",
+                "live.github.com",
+                "github.githubassets.com",
+                "central.github.com",
+                "desktop.githubusercontent.com",
+                "assets-cdn.github.com",
+                "camo.githubusercontent.com",
+                "github.map.fastly.net",
+                "github.global.ssl.fastly.net",
+                "gist.github.com",
+                "github.io",
+                "github.com",
+                "github.blog",
+                "api.github.com",
+                "raw.githubusercontent.com",
+                "user-images.githubusercontent.com",
+                "favicons.githubusercontent.com",
+                "avatars5.githubusercontent.com",
+                "avatars4.githubusercontent.com",
+                "avatars3.githubusercontent.com",
+                "avatars2.githubusercontent.com",
+                "avatars1.githubusercontent.com",
+                "avatars0.githubusercontent.com",
+                "avatars.githubusercontent.com",
+                "codeload.github.com",
+                "github-cloud.s3.amazonaws.com",
+                "github-com.s3.amazonaws.com",
+                "github-production-release-asset-2e65be.s3.amazonaws.com",
+                "github-production-user-asset-6210df.s3.amazonaws.com",
+                "github-production-repository-file-5c1aeb.s3.amazonaws.com",
+                "githubstatus.com",
+                "github.community",
+                "github.dev",
+                "collector.github.com",
+                "pipelines.actions.githubusercontent.com",
+                "media.githubusercontent.com",
+                "cloud.githubusercontent.com",
+                "objects.githubusercontent.com",
+                "vscode.dev"
+            ]
+    except:
+        raise ValueError(
+            '请配置正确的 site_list 参数，如 \'["site1.com","site2.com"]\'')
 
     # 设置hosts时，注释会被忽略掉
     new_custom_hosts = [
@@ -129,11 +181,11 @@ if __name__ == '__main__':
         # "127.0.0.1 le.dev.com # test",  # " # test" 这部分会被忽略
         # "# xxx",  # 整行会被忽略
     ]
-    
+
     get_custom_hosts_res = operate_custom_host('get')
     if type(get_custom_hosts_res) != dict or get_custom_hosts_res.get('code', -1) != 0:
-        print('更新异常 => ', get_custom_hosts_res)
-        exit(0)
+        print('更新失败 => ', get_custom_hosts_res)
+        raise Exception(get_custom_hosts_res)
 
     custom_hosts = get_custom_hosts_res.get('hosts', [])
     github_ips_start = '127.0.0.1 le.github-ips.start'
@@ -151,9 +203,10 @@ if __name__ == '__main__':
     custom_hosts = cover_ip(custom_hosts)
     # print('new custom_hosts => ', json.dumps(custom_hosts, indent=2))
 
-    github_ips = list(filter(lambda x: re.match(r'^\d', x), get_github_ips().split('\n')))
-    print('github_ips => ', json.dumps(github_ips, indent=2))
-    github_ips = cover_ip(github_ips)
+    # github_ips = list(filter(lambda x: re.match(r'^\d', x), get_github_ips().split('\n')))
+    # github_ips = get_github_ips()
+    # print('github_ips => ', json.dumps(github_ips, indent=2))
+    github_ips = cover_ip(site_list)
     print('new github_ips => ', json.dumps(github_ips, indent=2))
 
     # 添加 github_ips 内容的开始、结束元素，方便下次仅更新 github_ips 部分的内容
